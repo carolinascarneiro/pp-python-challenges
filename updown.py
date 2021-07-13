6dfbeb704982a47849eb9261949faab14b0c5155
carolinascarneiro <carneiro.carolina@yahoo.com.br>
2021-07-13 09:42:00 +0200

    more challenges

---
+++ updown.py
@@ -0,0 +1,17 @@
+# Challenge
+
+# Up and down
+# Define a function named up_down that takes a single number as its parameter. 
+# Your function return a tuple containing two numbers; 
+# the first should be one lower than the parameter, and the second should be one higher.
+# For example, calling up_down(5) should return (4, 6).
+
+#My solution
+def up_down(n):
+    print(tuple([n-1, n+1]))
+
+#PP Solution
+# def up_down(x):
+    # return (x-1, x+1) 
+
+
