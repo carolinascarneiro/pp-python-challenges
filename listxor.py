6e23511781cedb30b722eb22b4d577ceef220cac
carolinascarneiro <carneiro.carolina@yahoo.com.br>
2021-07-17 17:00:19 +0200

    adding new challenges

---
+++ listxor.py
@@ -0,0 +1,3 @@
+def list_xor(n, list1, list2):
+    if n in list 1 and not in list2 or n in list2 and not in list 1:
+        
--- validate.py
+++ validate.py
@@ -1,9 +1,26 @@
 def validate(s):
-    a = s.split(sep=" ")
-    check = ["def", ":", ""]
-    for i in check:
-        if i in a:
-            "must contain something"
+    a = s.split()
+    print(a)
+    print(a)
+    b = []
+    for item in a:
+    if "def" in item:
+        b.append("There's def")
+        elif ":" in item:
+            b. append("There's :")
+        elif "(" and ")" in item: #OK
+            b. append("There's ( and )")
+        elif "()" in item: #OK
+            b.append("all parameters are fine")
+        elif "    " in item: #NOT OK
+            b.append("not missing indent")
+        elif "validate" in item: #NOT OK
+            b.append("right name")
+        elif "return" in item: #OK
+            b.append("there's return")
+    print(b)
+    print(len(b))
 
-validate("def validate(s): a = s.split(sep=" ") print(a)")
+
+validate("def validate(s):   a = s.split(sep=" ") print(a) return")
 
